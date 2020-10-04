dc67cf6a0299be0e523fe6be2cee90abee3c1565
Kiranvamshi <kiranvamshi1996@gmail.com>
2020-10-04 19:09:41 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,24 +4,10 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
+//json//
+const budget = require('./myBudget.json');
 
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
 
 app.get ('/hello', (req, res) => {
